faca32164019feed723120ec8b6103505a56456a
Daniel Solis <dasolis50115@yahoo.com>
2026-07-11 17:18:24 -0400

    play button added and scene order

--- game/Menus/player_select.gd
+++ game/Menus/player_select.gd
@@ -1,11 +1,15 @@
 extends Control
 
 const MAIN_MENU_PATH: String = "res://game/Menus/main_menu.tscn"
+const GAME_MAP_PATH: String = "res://game/Maps/game_map.tscn"
 
 func _on_back_player_select_pressed() -> void:
 	get_tree().change_scene_to_file(MAIN_MENU_PATH)
 
-@onready var first_button: Button =  $CenterContainer/BackPlayerSelect
-
+@onready var first_button: Button =  $CenterContainer/HBoxContainer/BackPlayerSelect
 func _ready() -> void:
 	first_button.grab_focus.call_deferred()
+
+
+func _on_begin_pressed() -> void:
+	get_tree().change_scene_to_file(GAME_MAP_PATH)
